c0b0bfd6a219511438474add7a74417cc78a902d
Yananart <yananart@yananart.cn>
2022-09-09 18:13:52 +0800

    修改. 首页调整，添加头部展示框

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,13 +3,24 @@ import cacheUtil from '../../utils/cache'
 Page({
     data: {
         nickName: '',
-        avatarUrl: ''
+        avatarUrl: '',
+        year: '2022',
+        month: '09'
     },
     onShow() {
         let userInfo = cacheUtil.getUserInfo()
         this.setData({
             nickName: userInfo.nickName,
             avatarUrl: userInfo.avatarUrl
+        })
+    },
+    selectMonth(event) {
+        let month = event.detail.value
+        let year = month.substr(0, 4)
+        month = month.substr(5, 2)
+        this.setData({
+            year: year,
+            month: month
         })
     }
 })
