1f3b187f959c88124929abc845fdf3b3361be1c2
Yananart <yananart@yananart.cn>
2022-09-22 20:18:28 +0800

    :sparkles: 完成自定义键盘输入与运算逻辑，时间选择逻辑

--- pages/bill-edit/bill-edit.js
+++ pages/bill-edit/bill-edit.js
@@ -4,7 +4,7 @@ Page({
         select: 0,
         showEdit: false,
         selectType: {},
-        input: '',
+        input: '0',
         category: [
             [
                 {
@@ -117,25 +117,119 @@ Page({
             showEdit: true
         })
     },
+    /**
+     * 获取输入信息里面的最后一个数字
+     */
+    getInputLastNumber() {
+        const input = this.data.input
+        const length = input.length
+        const lastAt = length - 1
+        const indexPlus = input.indexOf('+')
+        const indexMinus = input.indexOf('-')
+        if (indexPlus >= 0 || indexMinus >= 0) {
+            if (indexPlus === lastAt || indexMinus === lastAt) {
+                return ''
+            }
+            if (indexPlus >= 0) return input.substring(indexPlus + 1, length)
+            else return input.substring(indexMinus + 1, length)
+        }
+        return input
+    },
+    calculateInput() {
+        const input = this.data.input
+        const length = input.length
+        const lastAt = length - 1
+        const indexPlus = input.indexOf('+')
+        const indexMinus = input.indexOf('-')
+        if (indexPlus >= 0 || indexMinus >= 0) {
+            if (indexPlus === lastAt || indexMinus === lastAt) {
+                return parseFloat(input.substring(0, lastAt))
+            }
+            let index;
+            if (indexPlus >= 0) index = indexPlus
+            else index = indexMinus
+            const first = parseFloat(input.substring(0, index))
+            const sencond = parseFloat(input.substring(index + 1, length))
+            if (indexPlus >= 0) return parseFloat((first + sencond).toFixed(2))
+            else return Math.abs(parseFloat((first - sencond).toFixed(2)))
+        }
+        return parseFloat(input)
+    },
     hitKeyboard(event) {
         const key = event.currentTarget.dataset.key;
         let input = this.data.input;
         if (key === this.data.keyboardKeys[14]) {
             input = input.substr(0, input.length - 1)
+            if (input.length === 0) {
+                input = '0'
+            }
+        } else if (key === '.') {
+            let last = this.getInputLastNumber()
+            if (last.indexOf('.') >= 0 || last.length === 0) return
+            input = input + key
+        } else if (key === '+' || key === '-') {
+            let lastAt = input.length - 1
+            const indexPlus = input.indexOf('+')
+            const indexMinus = input.indexOf('-')
+            if (indexPlus >= 0 || indexMinus >= 0) {
+                if (indexPlus == lastAt || indexMinus == lastAt) return
+                input = this.calculateInput() + key
+            } else {
+                input = input + key
+            }
         } else {
-            input = input + key;
+            let last = this.getInputLastNumber()
+            if (key === '0') {
+                let last = this.getInputLastNumber()
+                if (last === '0' || last === '0.0') return
+            }
+            if (last === '0') {
+                input = input.substring(0, input.length - 1) + key
+            } else if (last.length > 0) {
+                const indexPoint = last.indexOf('.')
+                if (indexPoint >= 0) {
+                    if (last.length - indexPoint > 2) return
+                } else {
+                    if (parseFloat(last) >= 10000000) return
+                }
+                input = input + key
+            } else {
+                input = input + key
+            }
         }
+
         this.setData({
             input: input
         })
     },
-    choseDay() {
-        console.log('选择日期');
+    choseDay(event) {
+        let date = event.detail.value
+        const now = new Date()
+        const today = now.getFullYear() + '/' + (now.getMonth() + 1).toString().padStart(2, '0') + '/' + now.getDate().toString().padStart(2, '0')
+        date = date.replace(/-/g, '/')
+        if (today === date) {
+            date = '今天'
+        }
+        this.setData({
+            'keyboardKeys[3]': date
+        })
     },
     finishEdit() {
-        wx.showToast({
-            title: '还没做提交',
-            icon: 'error'
-        })
+        const amount = this.calculateInput()
+        if (amount === 0) {
+            wx.showToast({
+                title: '没有输入金额',
+                icon: 'error'
+            })
+        } else {
+            wx.showToast({
+                title: '账单' + amount + '元',
+                icon: 'success'
+            })
+            this.setData({
+                showEdit: false,
+                input: '0'
+            })
+        }
     }
 })
